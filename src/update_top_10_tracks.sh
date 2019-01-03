3511388f0860782f245e98ae80926193da348032
dchege711 <d.chege711@gmail.com>
2019-01-03 10:11:32 -0500

    Clean the log files before running the updater script

--- src/update_top_10_tracks.sh
+++ src/update_top_10_tracks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Updates my top 10 songs
+# Clean the log files
+> logs/stdout.txt
+> logs/stderr.txt
 
+# Updates my top 10 songs
 python music_chart.py
 git add --all
 git commit -m "Update top 10 songs"
