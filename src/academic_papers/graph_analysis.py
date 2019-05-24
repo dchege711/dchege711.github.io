dd70730e8612c253c2aa2d0c51e8fbe8e44c33bf
dchege711 <d.chege711@gmail.com>
2019-05-24 00:01:02 -0400

    Update top 10 songs

--- src/academic_papers/graph_analysis.py
+++ src/academic_papers/graph_analysis.py
@@ -1,14 +1,26 @@
 import sqlite3
 import networkx as nx
+import os
+import pickle
 
-conn = sqlite3.connect("/Users/dchege711/datasets/citations_data/papers.db")
+db_path = "/Users/dchege711/datasets/citations_data/papers.db"
+conn = sqlite3.connect(db_path)
 conn.row_factory = sqlite3.Row
 
-def get_references_graph():
+def get_references_graph(use_cache=True):
     """
     Generate a graph object where nodes are paper ids and edge `(u, v)` means 
     that paper `u` cited paper `v`
     """
+
+    cache_path = "./cache/reference_graph.pickle"
+    if use_cache:
+        if os.path.exists(cache_path):
+            cache_mtime = os.path.getmtime(cache_path)
+            db_mtime = os.path.getmtime(db_path)
+            if db_mtime < cache_mtime:
+                return pickle.load(open(cache_path, "r"))
+
     G = nx.DiGraph()
 
     print("Creating the graph of references...")
@@ -27,6 +39,10 @@ def get_references_graph():
             G.add_node(row["paper_id"], title=row["title"])
         else:
             G.nodes[row["paper_id"]]["title"] = row["title"]
+    
+    print("Caching the graph object...")
+    with open(cache_path, "w") as f:
+        pickle.dump(G, f)
         
     return G
 
