b9046e51ddb0dcd7a62d6df0adf22f072c72e2f0
dchege711 <d.chege711@gmail.com>
2019-06-22 20:19:34 -0400

    Compute PageRank of publications using networkx

--- src/academic_papers/graph_analysis.py
+++ src/academic_papers/graph_analysis.py
@@ -1,54 +1,84 @@
 import sqlite3
-import os
-import pickle
-from cProfile import Profile
 from typing import List
+from operator import itemgetter
+from cProfile import Profile
+from pstats import Stats
+import json
 
 import networkx as nx
+from networkx.readwrite import json_graph
 import numpy as np
 from sklearn.preprocessing import normalize
+from scipy.sparse import csr_matrix, lil_matrix
 
 db_path = "/Users/dchege711/datasets/citations_data/papers.db"
 conn = sqlite3.connect(db_path)
 conn.row_factory = sqlite3.Row
 
-def get_references_graph(use_cache=True):
+CACHE_FILEPATH = "./__cache__/citation_graph_cache.json"
+CITATIONS_QUERY = " ".join([
+    "SELECT paper_references.paper_id, paper_references.cited_paper_id",
+    "FROM paper_references, papers",
+    "WHERE paper_references.paper_id = papers.paper_id AND papers.n_citation >= ?;"
+])
+MIN_NUM_CITATIONS = 100
+
+def cache_graph(G):
+    g_data = json_graph.node_link_data(G)
+    data_to_cache = {
+        "query": CITATIONS_QUERY, "graph": g_data
+    }
+    with open(CACHE_FILEPATH, "w") as fp:
+        json.dump(data_to_cache, fp)
+
+def retrieve_graph_from_cache():
+    try: fp = open(CACHE_FILEPATH, "r")
+    except FileNotFoundError: return None
+
+    cached_data = json.load(fp)
+    if cached_data["query"] == CITATIONS_QUERY: 
+        print("Retrieved citations graph from cache!")
+        return json_graph.node_link_graph(cached_data["graph"])
+    else: return None
+
+def get_citation_graph():
     """
     Generate a graph object where nodes are paper ids and edge `(u, v)` means 
     that paper `u` cited paper `v`
     """
-
-    cache_path = "./cache/reference_graph.pickle"
-    if use_cache:
-        if os.path.exists(cache_path):
-            cache_mtime = os.path.getmtime(cache_path)
-            db_mtime = os.path.getmtime(db_path)
-            if db_mtime < cache_mtime:
-                return pickle.load(open(cache_path, "r"))
-
+    G = retrieve_graph_from_cache()
+    if G is not None: return G
+    
     G = nx.DiGraph()
-
-    print("Creating the graph of references...")
-    cursor = conn.execute(
-        "SELECT paper_id, cited_paper_id FROM paper_references;"
-    )
+    cursor = conn.execute(CITATIONS_QUERY, [MIN_NUM_CITATIONS])
     for row in cursor:
+        for paper_id in [row["paper_id"], row["cited_paper_id"]]:
+            if paper_id not in G:
+                G.add_node(paper_id)
+                paper_info = conn.execute(
+                    """
+                    SELECT papers.title, papers.year, papers.n_citation  
+                    FROM papers WHERE papers.paper_id = ? 
+                    """, [paper_id]
+                ).fetchone()
+                paper_info_dict = {}
+                for key in paper_info.keys():
+                    G.nodes[paper_id][key] = paper_info[key]
+
         G.add_edge(row["paper_id"], row["cited_paper_id"])
 
-    print("Adding title information...")
+    # Append author information for easier Google searches
     cursor = conn.execute(
-        "SELECT paper_id, title FROM papers;"
+        """
+        SELECT authors.author_name, authors.paper_id 
+        FROM authors WHERE authors.appearance_order = 1;
+        """
     )
     for row in cursor:
-        if row["paper_id"] not in G:
-            G.add_node(row["paper_id"], title=row["title"])
-        else:
-            G.nodes[row["paper_id"]]["title"] = row["title"]
-    
-    print("Caching the graph object...")
-    with open(cache_path, "w") as f:
-        pickle.dump(G, f)
-        
+        if row["paper_id"] in G:
+            G.nodes[row["paper_id"]]["author_name"] = row["author_name"]
+
+    cache_graph(G)
     return G
 
 def cycle_analysis(G):
@@ -60,69 +90,61 @@ def cycle_analysis(G):
             print(node["title"])
         print("______________________")
 
-def get_normalized_adjacency_citation_matrix():
+def get_citations_adjacency_matrix() -> (lil_matrix, dict):
     """
-    Construct the transitional matrix M, where `M[i][j]` represents the link 
-    from `j` to `i` such that for all `j`, `sum(i, M[i][j]) = 1`
+    Construct the adjacency matrix for the citation data. The biggest limitation 
+    is efficiency.
+
+    | Filter | Time (Seconds) |
+    | --- | --- |
+    | any citation | 338.707 | 
+    | citations from papers with >= 100 citations | 61.387 |
 
     """
-    cursor = conn.execute("SELECT count(papers.paper_id) from papers;")
-    N = cursor.fetchone()["count(papers.paper_id)"]
 
     paper_id_to_idx_mapping = {}
-    cursor = conn.execute(
-        "SELECT paper_id, cited_paper_id FROM paper_references;"
-    )
-    idx = -1
-    adjacency_matrix = np.zeros((N, N))
-    for row in cursor:
-        if row["paper_id"] not in paper_id_to_idx_mapping:
-            idx += 1
-            paper_id_to_idx_mapping[row["paper_id"]] = idx
+    for row in conn.execute(CITATIONS_QUERY, [MIN_NUM_CITATIONS]):
+        for paper_id in [row["paper_id"], row["cited_paper_id"]]:
+            if paper_id not in paper_id_to_idx_mapping:
+                paper_id_to_idx_mapping[paper_id] = len(paper_id_to_idx_mapping)
 
-        if row["cited_paper_id"] not in paper_id_to_idx_mapping:
-            idx += 1
-            paper_id_to_idx_mapping[row["cited_paper_id"]] = idx
-        
+    N = len(paper_id_to_idx_mapping)
+    print("The matrix is {:,} x {:,} ...".format(N, N))
+    adjacency_matrix = lil_matrix((N, N), dtype="d")
+    
+    cursor = conn.execute(CITATIONS_QUERY, [MIN_NUM_CITATIONS])
+    for row in cursor:
         i = paper_id_to_idx_mapping[row["cited_paper_id"]]
         j = paper_id_to_idx_mapping[row["paper_id"]]
         adjacency_matrix[i, j] = 1
 
-    adjacency_matrix = normalize(adjacency_matrix, norm="l1", axis=0, copy=False)
     return adjacency_matrix, paper_id_to_idx_mapping
 
-def calculate_pagerank(M, d=0.85, eps=1.0e-8) -> List[float]:
-    """
-    Calculate the page rank of the various articles. Adapted from 
-    https://en.wikipedia.org/wiki/PageRank#Python
-    
-    :kwarg d: ``float`` Damping factor
+def run_nx_analysis():
+    print("Fetching the citation graph...")
+    G = get_citation_graph()
 
-    :kwarg eps: ``float`` The quadratic error for the vector of ranks, `v`
-
-    :return List[float]: A vector of ranks such that `v[i]` is the rank 
-    of the `i`-th paper from `[0, 1]`
-    """
-
-    N = M.shape[1]
-    v = np.random.rand(N, 1)
-    v = v / np.linalg.norm(v, 1)
-    last_v = np.ones((N, 1), dtype=np.float32) * 100
-    
-    while np.linalg.norm(v - last_v, 2) > eps:
-        last_v = v
-        v = d * np.matmul(M, v) + (1 - d) / N
-    return v
-
-def run_pagerank():
-    print("Fetching the normalized citations adjacency matrix...", end=" ")
-    M, mapping = get_normalized_adjacency_citation_matrix()
+    print("\n---------------------------------\n")
+    print("Here is the pagerank analysis...\n")
+    pr = [(paper_id, rank) for (paper_id, rank) in nx.pagerank(G).items()]
+    pr.sort(key=itemgetter(1), reverse=True)
+    for paper_id, rank in pr[:15]:
+        G.nodes[paper_id]["pagerank"] = rank
+        print(
+            " ".join(
+                str(x) for x in G.nodes[paper_id].values()
+            )
+        )
+    print("\n---------------------------------\n")
 
 if __name__ == "__main__":
     pr = Profile()
     pr.enable()
 
-    run_pagerank()
+    # run_pagerank()
+    run_nx_analysis()
 
     pr.disable()
-    pr.print_stats()
+    ps = Stats(pr)
+    ps.sort_stats("cumulative")
+    ps.print_stats(0.1)
