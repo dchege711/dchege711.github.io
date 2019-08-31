2beca16fe0ba989c17173d666b5c29b9969c576f
dchege711 <d.chege711@gmail.com>
2019-08-31 11:52:03 -0700

    Update my flash cards

--- src/com.c13u.cards/update_flash_cards.sh
+++ src/com.c13u.cards/update_flash_cards.sh
@@ -5,7 +5,6 @@
 > logs/stderr.txt
 
 # Updates my flash cards
-conda activate /Users/dchege711/dchege711.github.io/src/com.c13u.cards/com.c13u.cards.venv
 python fetch_recent_cards.py
 git add --all
 git commit -m "Update my flash cards"
